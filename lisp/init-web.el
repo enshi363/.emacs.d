86db0612c7c83191745ff8670115100ebaaafc9c
施恩 <enshi363@qq.com>
2018-02-27 22:32:54 +0800

    add emmet 	modified:   lisp/init-web.el

--- lisp/init-web.el
+++ lisp/init-web.el
@@ -1,4 +1,9 @@
 (require-package 'web-mode)
+(require-package 'emmet-mode)
+
+(add-hook 'html-mode-hook 'emmet-mode)
+(add-hook 'web-mode-hook 'emmet-mode)
+(add-hook 'css-mode-hook  'emmet-mode)
 
 
 (provide 'init-web)
